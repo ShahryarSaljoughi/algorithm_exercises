fc9cfe7718a22e8fb98e379eeed1705943afaa58
shahryar_slg <s.shahryar75@gmail.com>
2016-12-16 21:46:57 +0330

    done!

--- algorithm_exercises/maximum_subarray_brute_force/msabf.py
+++ algorithm_exercises/maximum_subarray_brute_force/msabf.py
@@ -17,15 +17,23 @@ def compute_maximum_subarray(A):
     """
 
     :param A: A is a list , containing some numbers
-    :return: (i, j) will be returned , i is the index of the start of Maximum-subarray,
-     and j refers to the end of subarray
+    :return: (i, j, max_sum) will be returned , i is the index of the start of Maximum-subarray,
+     and j refers to the end of subarray,
+     max_sum is the sum of the numbers of subarray
     """
 
-    max_subarray = (0, 0)
+    max_subarray = (0, 0)  # (i, j)
     max_sum = A[0]
 
-    for i in range(len(A)):
-        for j in range(i, len(A)):
-            sum = compute_sum(A[i:j])
-            
+    for i in range(len(A)):  # i is the start index
+        for j in range(i, len(A)):  # j is the end index
+            subarray_sum = compute_sum(A[i:j])
+            if subarray_sum > max_sum:
+                max_sum = subarray_sum
+                max_subarray = (i, j)
+    return max_subarray, max_sum
 
+if __name__ == '__main__':
+    a = eval(raw_input(">>input array. i.e: [1,-12, 10, 15, -2, 6]"))
+    b = compute_maximum_subarray(a)
+    print b
